b1f8ff1d15ff423b48b2f3079d2f1d83225fd7ac
Andrew <a.macmurray@icloud.com>
2016-01-27 14:05:48 +0000

    add buttons, draft basic function outline

---
+++ stopwatch.js
@@ -0,0 +1,29 @@
+// function start() {
+//     // maybe use setinterval to refresh page
+//     // need to return date as string?
+//     var beginning = Date.now();
+//     setTimeout(function() {
+//         var end = Date.now();
+//         var diff = (end - beginning);
+//         console.log(diff);
+//     }, 1000);
+//
+//     return 0;
+// }
+
+var Stopwatch = {
+    function: increment() {
+        function add() {
+
+        }
+    },
+    function: clearTimeout() {
+
+    },
+    function: start() {
+
+    },
+    function: stop() {
+
+    }
+};
