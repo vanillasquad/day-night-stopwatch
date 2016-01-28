2aed239b7ef90bb0506404f6342b6a5fe75d3421
Katherine Bowler <katherine.bowler@gmail.com>
2016-01-28 16:51:19 +0000

    Add lap button functionality

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ test('increment should increase csecs', function(assert) {
     Timer.start();
     setTimeout(function() {
         var result = Timer.displayTime();
-        var actual = result[result.length-2];
-        assert.ok(actual, actual > 0);
+        var actual = result[result.length-2] + result[result.length-1];
+        assert.ok(actual > 0);
         Timer.reset();
         done();
-    }, 100);
+    }, 85);
 });
 
 test('increment should increase secs', function(assert) {
@@ -16,7 +16,7 @@ test('increment should increase secs', function(assert) {
     setTimeout(function() {
         var result = Timer.displayTime();
         var actual = result[result.length-4];
-        assert.ok(actual, actual > 0);
+        assert.ok(actual > 0);
         Timer.reset();
         done();
     }, 2000);
@@ -39,8 +39,8 @@ test('pause timer when stop is clicked', function(assert) {
     Timer.start();
     setTimeout(function() {
         var result = Timer.displayTime();
-        var actual = result[result.length-4] + result[result.length-2];
-        assert.equal(actual, '10');
+        var actual = parseInt(result[result.length-4] + result[result.length-2]);
+        assert.ok(actual);
         Timer.stop();
         done();
     }, 1000);
@@ -79,3 +79,109 @@ test('clicking the reset button after pressing start then stop resets timer to 0
         done();
     }, 2000);
 });
+
+test('clicking the moon should switch to night mode', function(assert) {
+    document.getElementById('moon').click();
+    var actual = document.body.className;
+    var expected = 'night';
+    var done = assert.async();
+    setTimeout(function() {
+        done();
+    }, 1500);
+    assert.equal(actual, expected);
+});
+
+test('clicking the sun should switch to day mode', function(assert) {
+    document.getElementById('sun').click();
+    var actual = document.body.className;
+    var expected = '';
+    assert.equal(actual, expected);
+});
+
+test('clicking lap logs the elapsed time', function(assert) {
+    Timer.start();
+    var done = assert.async();
+    setTimeout(function() {
+        var actual = !!document.getElementsByClassName('lapcount');
+        Timer.lap();
+        assert.ok(actual);
+        done();
+        Timer.reset();
+    }, 3000);
+});
+
+test('clicking lap should reset timer to zero', function(assert) {
+    Timer.start();
+    var done = assert.async();
+    setTimeout(function() {
+        Timer.lap();
+        var actual = Timer.displayTime();
+        assert.equal(actual, '00:00:00:00');
+        done();
+    }, 2000);
+});
+
+test('clicking lap restarts timing after reset', function(assert) {
+    var done = assert.async();
+    Timer.start();
+    setTimeout(function() {
+        Timer.lap();
+        setTimeout(function() {
+            var result = Timer.displayTime();
+            var actual = result[result.length-2] + result[result.length-1];
+            assert.ok(actual > 0);
+            Timer.reset();
+            done();
+        }, 80);
+    }, 85);
+});
+
+test('clicking lap more than once adds more laps', function(assert) {
+    Timer.start();
+    var done = assert.async();
+    setTimeout(function() {
+        Timer.lap();
+        var actual = document.getElementsByClassName('lapcount');
+        assert.ok(actual.length > 1);
+        done();
+    }, 2000);
+});
+
+test('lap button is hidden when timer is stopped', function(assert) {
+    Timer.stop();
+    var actual = document.getElementById('lap').className === 'btn hide-btn';
+    assert.ok(actual);
+});
+
+test('lap button is hidden when timer is reset', function(assert) {
+    Timer.reset();
+    var actual = document.getElementById('lap').className === 'btn hide-btn';
+    assert.ok(actual);
+});
+
+test('start button is hidden when timer is running', function(assert) {
+    Timer.start();
+    var actual = document.getElementById('start').className === 'btn hide-btn';
+    assert.ok(actual);
+});
+
+test('start button should appear when timer is stopped', function(assert) {
+    Timer.stop();
+    var actual = document.getElementById('start').className === 'btn';
+    assert.ok(actual);
+});
+
+test('start button should appear when timer is reset', function(assert) {
+    Timer.reset();
+    var actual = document.getElementById('start').className === 'btn';
+    assert.ok(actual);
+});
+
+test('lap button should appear when timer is running', function(assert) {
+    Timer.start();
+    var actual = document.getElementById('lap').className === 'btn';
+    assert.ok(actual);
+});
+
+// add lap number?
+// reset function removes laps
