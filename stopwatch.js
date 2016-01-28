d230379afebd03d861d5f7ff76e4b07a2da38300
Andrew <a.macmurray@icloud.com>
2016-01-28 12:03:22 +0000

    modularise code to hide global variables
    refactor tests to fit the module pattern

--- stopwatch.js
+++ stopwatch.js
@@ -1,61 +1,81 @@
-var started = false;
-document.getElementById('start').onclick = start;
-function start() {
-    if (!started) {
-        started = true;
-    }
-}
-
-document.getElementById('stop').onclick = stop;
-function stop() {
-    if (started) {
-        started = false;
-    }
-}
-
-var csecs = 0;
-var secs = 0;
-var mins = 0;
-var hrs = 0;
-
-setInterval(increment, 10);
-function increment() {
-    if (started) {
-        csecs++;
-        if (csecs > 99) {
-            csecs = 0;
-            secs++;
+var Timer = (function (){
+    var started = false;
+    var start = function() {
+        if (!started) {
+            started = true;
         }
-        if (secs > 59) {
-            secs = 0;
-            mins++;
-        }
-        if (mins > 59) {
-            mins = 0;
-            hrs++;
-        }
-        display.innerHTML = displayTime();
-        return displayTime();
-    }
-}
+    };
 
-var display = document.getElementById('time');
-var defaultDisplay = '00:00:00:00';
-display.innerHTML = defaultDisplay;
-function displayTime() {
-    var displayhours = hrs < 10 ? '0' + hrs : hrs;
-    var displaymins = mins < 10 ? '0' + mins : mins;
-    var displaysecs = secs < 10 ? '0' + secs : secs;
-    var displaycsecs = csecs < 10 ? '0' + csecs : csecs;
-    return displayhours + ':' + displaymins + ':' + displaysecs + ':' + displaycsecs;
-}
+    var stop = function() {
+        if (started) {
+            started = false;
+        }
+    };
 
-document.getElementById('reset').onclick = reset;
-function reset() {
-    hrs = 0;
-    mins = 0;
-    secs = 0;
-    csecs = 0;
+    var csecs = 0;
+    var secs = 0;
+    var mins = 0;
+    var hrs = 0;
+
+    var startClock = function() {
+        return window.setInterval(Timer.increment, 10);
+    };
+    var increment = function() {
+        console.log('started');
+        if (started) {
+            csecs++;
+            if (csecs > 99) {
+                csecs = 0;
+                secs++;
+            }
+            if (secs > 59) {
+                secs = 0;
+                mins++;
+            }
+            if (mins > 59) {
+                mins = 0;
+                hrs++;
+            }
+            display.innerHTML = displayTime();
+            return displayTime();
+        }
+    };
+
+    var display = document.getElementById('time');
+    var defaultDisplay = '00:00:00:00';
     display.innerHTML = defaultDisplay;
-    started = false;
-}
+    var displayTime = function() {
+        var displayhours = hrs < 10 ? '0' + hrs : hrs;
+        var displaymins = mins < 10 ? '0' + mins : mins;
+        var displaysecs = secs < 10 ? '0' + secs : secs;
+        var displaycsecs = csecs < 10 ? '0' + csecs : csecs;
+        return displayhours + ':' + displaymins + ':' + displaysecs + ':' + displaycsecs;
+    };
+
+    var reset = function() {
+        hrs = 0;
+        mins = 0;
+        secs = 0;
+        csecs = 0;
+        display.innerHTML = defaultDisplay;
+        started = false;
+    };
+    return {
+        startClock: startClock,
+        start: start,
+        stop: stop,
+        increment: increment,
+        displayTime: displayTime,
+        started: started,
+        reset: reset,
+        csecs: csecs,
+        secs: secs,
+        mins: mins,
+        hrs: hrs
+    };
+}());
+
+Timer.startClock();
+document.getElementById('start').onclick = Timer.start;
+document.getElementById('stop').onclick = Timer.stop;
+document.getElementById('reset').onclick = Timer.reset;
--- test/test.js
+++ test/test.js
@@ -1,76 +1,80 @@
 test('increment should increase csecs', function(assert) {
     var done = assert.async();
-    start();
+    Timer.start();
     setTimeout(function() {
-        var actual = csecs;
+        var result = Timer.displayTime();
+        var actual = result[result.length-2];
         assert.ok(actual, actual > 0);
-        reset();
+        Timer.reset();
         done();
     }, 100);
 });
 
 test('increment should increase secs', function(assert) {
     var done = assert.async();
-    start();
+    Timer.start();
     setTimeout(function() {
-        var actual = secs;
+        var result = Timer.displayTime();
+        var actual = result[result.length-4];
         assert.ok(actual, actual > 0);
-        reset();
+        Timer.reset();
         done();
     }, 2000);
 });
 
 test('increment should increase by 5 secs', function(assert) {
     var done = assert.async();
-    start();
+    Timer.start();
     setTimeout(function() {
-        var actual = secs;
+        var result = Timer.displayTime();
+        var actual = result[result.length-4];
         assert.equal(actual, 5);
-        reset();
+        Timer.reset();
         done();
     }, 5010);
 });
 
 test('pause timer when stop is clicked', function(assert) {
     var done = assert.async();
-    start();
+    Timer.start();
     setTimeout(function() {
-        var actual = displayTime();
-        assert.equal(actual, '00:00:01:00');
-        stop();
+        var result = Timer.displayTime();
+        var actual = result[result.length-4] + result[result.length-2];
+        assert.equal(actual, '10');
+        Timer.stop();
         done();
     }, 1000);
 });
 
 test('clicking the reset button resets timer to 00:00:00:00', function(assert) {
     var done = assert.async();
-    start();
+    Timer.start();
     setTimeout(function() {
-      reset();
-      var actual = display.innerHTML;
+      Timer.reset();
+      var actual = document.getElementById('time').innerHTML;
       assert.equal(actual, '00:00:00:00');
       done();
    }, 2000);
 });
 
 test('clicking the reset button after pressing start resets timer to 00:00:00:00', function(assert) {
-    start();
+    Timer.start();
     var done = assert.async();
     setTimeout(function() {
-        reset();
-        var actual = displayTime();
+        Timer.reset();
+        var actual = Timer.displayTime();
         assert.equal(actual, '00:00:00:00');
         done();
     }, 2000);
 });
 
 test('clicking the reset button after pressing start then stop resets timer to 00:00:00:00', function(assert) {
-    start();
+    Timer.start();
     var done = assert.async();
     setTimeout(function() {
-        stop();
-        reset();
-        var actual = displayTime();
+        Timer.stop();
+        Timer.reset();
+        var actual = Timer.displayTime();
         assert.equal(actual, '00:00:00:00');
         done();
     }, 2000);
